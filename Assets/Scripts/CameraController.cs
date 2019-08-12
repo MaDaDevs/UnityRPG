cfcd0e0be9db91d6c13decf1e2e7ddc2a492d1fa
root <root@DESKTOP-V07H129.localdomain>
2019-08-12 08:21:23 +0200

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+
+    public Tilemap theMap;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerController.instance.transform;
+
+        bottomLeftLimit = theMap.localBounds.min;
+        topRightLimit = theMap.localBounds.max;
+    }
+
+    // LateUpdate is called once per frame after Update
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
